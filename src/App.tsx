58a87cc5bcfa7bf34a58596c1603cbd19391ed56
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-12 16:21:25 +0300

    refactor: move header into Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import AboutUsPage from './pages/AboutUsPage';
 import HomePage from './pages/HomePage';
 import Page404 from './pages/Page404';
-import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUsPage />} />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="about" element={<AboutUsPage />} />
             <Route path="*" element={<Page404 />} />
-          </Routes>
-        </div>
+          </Route>
+        </Routes>
       </>
     );
   }
