d5957f0992f0768fa8fa718750382a1711d0793f
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-13 12:53:11 +0300

    feat: add header dynamic current page title

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ import Layout from './components/Layout';
 export default class App extends Component {
   render() {
     return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="about" element={<AboutUsPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Route>
-        </Routes>
-      </>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
     );
   }
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
+import { Routes, Route } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
@@ -8,6 +9,13 @@ export default class Header extends Component {
       <header className={s.header}>
         <div className="container">
           <nav className={s.nav}>
+            <h2>
+              <Routes>
+                <Route path="/" element={'Home'} />
+                <Route path="/about" element={'About Us'} />
+                <Route path="*" element={'Not Found'} />
+              </Routes>
+            </h2>
             <ul className={s.navList}>
               <li className={s.navItem}>
                 <NavLink to="/" className={s.navLink}>
