cb4824326de98d848ad841961ecbb50d3c42f1b6
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-14 12:36:23 +0300

    feat: add localStorage set/get logic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import s from './SearchBar.module.css';
 
-export default class SearchBar extends Component {
+interface SearchBarState {
+  searchValue: string;
+}
+
+export default class SearchBar extends Component<SearchBarState> {
+  state = {
+    searchValue: localStorage.getItem('productSearchValue') || '',
+  };
+
+  handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    this.setState({ searchValue: value });
+  };
+
+  handleRefresh = () => {
+    localStorage.setItem('productSearchValue', this.state.searchValue);
+  };
+
+  componentDidMount(): void {
+    const val = localStorage.getItem('productSearchValue');
+    if (val) {
+      this.setState({ searchValue: val });
+    }
+    window.addEventListener('beforeunload', this.handleRefresh);
+  }
+
+  componentWillUnmount(): void {
+    localStorage.setItem('productSearchValue', this.state.searchValue);
+    window.removeEventListener('beforeunload', this.handleRefresh);
+  }
+
   render() {
     return (
       <div className={s.searchWrapper}>
         <input
           type="search"
           className={s.searchInput}
-          // value={searchVal}
-          // onChange={(event) => {
-          //   const value = event.target?.value;
-          //   setSearchVal(value);
-          //   updateFilterState(value.toLocaleLowerCase());
-          // }}
+          value={this.state.searchValue}
+          onChange={this.handleSearchChange}
           required
         />
         <span className={s.searchTitle}>Search</span>
