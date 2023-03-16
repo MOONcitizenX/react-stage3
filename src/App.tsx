269de6682b495d79bbe42585bfd10189029b681f
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-16 18:30:13 +0300

    refactor: move BrowserRouter into App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import AboutUsPage from './pages/AboutUsPage';
 import HomePage from './pages/HomePage';
 import Page404 from './pages/Page404';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import Layout from './components/Layout';
 
 export default class App extends Component {
   render() {
     return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutUsPage />} />
-          <Route path="404" element={<Page404 />} />
-          <Route path="*" element={<Navigate to="404" />} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="about" element={<AboutUsPage />} />
+            <Route path="404" element={<Page404 />} />
+            <Route path="*" element={<Navigate to="404" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
