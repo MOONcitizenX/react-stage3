b37553ba9cec4a3cdedc425af2540e52b404d4c7
MOONcitizenX <mooncitizenx@gmail.com>
2023-03-24 09:37:43 +0300

    config: add aliases for imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages'),
+      assets: path.resolve(__dirname, './src/assets'),
+      data: path.resolve(__dirname, './src/data'),
+    },
+  },
   server: {
     open: true,
   },
